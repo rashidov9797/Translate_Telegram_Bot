5fd565f4d6c52db6ce7dba21598f173ee277d63e
rashidov9797 <78200967+rashidov9797@users.noreply.github.com>
2021-09-04 16:49:25 +0900

    1

---
+++ Telegram_bot/translate.py
@@ -0,0 +1,15 @@
+
+
+from googletrans import Translator
+
+
+
+def translate(matn):
+  translator = Translator()
+  til = translator.detect(matn).lang
+
+  if til == 'en':   #agar matn ingliz tilida bo'lsa
+    return translator.translate(matn,dest='uz').text
+  else:
+    return translator.translate(matn,dest='en').text  
+
